a709c1fc7c0a0183977ea1acbaac863d7c3b6f3c
smmanrrique <smmanrrique@hiberus.com>
2022-02-27 12:28:51 +0100

    feat(): added new variables to output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
+output "name" {
+  value = azurerm_application_insights.application_insights.name
+}
 output "instrumentation_key" {
   value = azurerm_application_insights.application_insights.instrumentation_key
 }
